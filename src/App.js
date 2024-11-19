7e3cc9b3933bef36b764e4e41037c20c75d08f6e
themiya-420 <themiya420@gmail.com>
2024-11-19 13:16:53 +0530

    Routed everything created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./views/Home";
+import Contact from "./views/Contact";
+import AboutUs from "./views/AboutUs";
+import Login from "./views/Login";
+import Signup from "./views/SignUp";
 
 function App() {
   return (
@@ -8,6 +12,10 @@ function App() {
       <Router>
         <Routes>
           <Route exact path="/" element={<Home />} />
+          <Route exact path="/about" element={<AboutUs />} />
+          <Route exact path="/contact" element={<Contact />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/signup" element={<Signup />} />
         </Routes>
       </Router>
     </div>
